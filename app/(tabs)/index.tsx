1f3599809032998f414fd297a2aa1861f09f1b7a
bekamDev <122617910+bekamDev@users.noreply.github.com>
2024-01-02 14:46:31 +0100

    Add files via upload

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { SafeAreaView, StyleSheet, Text, ScrollView } from 'react-native';
+
+import EditScreenInfo from '../../components/EditScreenInfo';
+
+export default function TabOneScreen() {
+  return (
+    <SafeAreaView>
+      <ScrollView className="flex">
+        <Text>Yamete chorasia</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+
