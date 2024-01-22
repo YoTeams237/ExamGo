432c946edd8f2de3793fc3906f6c45bec5798620
bekamDev <122617910+bekamDev@users.noreply.github.com>
2024-01-22 01:29:53 +0100

    setting up route

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,39 +1,11 @@
 import React, { useEffect } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
-import EditScreenInfo from "../../components/EditScreenInfo";
-import { Text, View } from "../../components/Themed";
-import SplashScreen from "react-native-splash-screen";
-import AppSplashScreen from "../SplashScreen";
-import HeroSection from "../../components/hero/HeroSection";
-export default function TabOneScreen() {
-  //  useEffect(() => {
-  //   setTimeout(() => {
-  //     SplashScreen?.hide(); // Add the null check here
-  //   }, 2000); // Adjust the delay as needed
-  // }, []);
+import HomeScreen from "../HomeScreen";
 
+export default function TabOneScreen() {
   return (
     <>
-      {/* <AppSplashScreen /> */}
-
-      <HeroSection/>
+      <HomeScreen />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
